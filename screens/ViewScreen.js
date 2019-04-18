e210a4e003183b15fdb8dd2e434b79f5e302a5f5
sam <sam.albasateneh@gmail.com>
2019-04-18 14:11:31 -0700

    fixed navigation, made map full screen and as homescreen

---
+++ screens/ViewScreen.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import {Text, View, Button} from 'react-native'
+export default class ViewScreen extends React.Component {
+  static navigationOptions = {
+    title: 'app.json',
+  };
+
+  render() {
+    /* Go ahead and delete ExpoConfigView and replace it with your
+     * content, we just wanted to give you a quick view of your config */
+    return <ExpoConfigView />;
+  }
+}
