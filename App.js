847e1dc3ff6e0c9e1efd91b3f4960a2209c3810f
Mike Shigo <mikeshigo@gmail.com>
2019-04-22 21:15:21 -0700

    Map is only visible after user is authenticated

--- App.js
+++ App.js
@@ -1,26 +1,47 @@
 import React from 'react';
 import { StyleSheet, View, Text } from 'react-native';
+
+import Amplify, { Auth } from 'aws-amplify'
+import awsmobile from './aws-exports'
+Amplify.configure(awsmobile)
+import Tabs from './components/Tabs'
 import Container from './navigation/Navigation'
+
 export default class App extends React.Component {
+  state = {
+    isAuthenticated: false
+  }
+  authenticate(isAuthenticated){
+    this.setState({
+      isAuthenticated
+    })
+  }
   render() {
+    if (this.state.isAuthenticated) {
+      console.log("Auth: ", Auth)
+      return(
+        // <View style={{flex:1, justifyContent: 'center', alignItems: 'center'}}>
+        //   <Text>Hello {Auth.user.username}!</Text>
+        // </View>
+        <Container />
+      )
+    }
     return (
-  
-      <Container />
+      <View style={styles.container}>
+        <Tabs 
+        screenProps={{
+          authenticate: this.authenticate.bind(this)
+        }}
+        />
+      </View>
     );
   }
 }
 
-
-  
-
-  
-
-  
-
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#000000',
+    backgroundColor: '#424242',
   },
 });
 
